30deb9ee8a99aceca26d90bfe63d40101ea35f79
Ahmed Mohamed <ahmedabozied699@gmail.com>
2023-05-23 18:22:59 +0300

    add loginUser controller

--- server/Controllers/UserControllers.js
+++ server/Controllers/UserControllers.js
@@ -1,26 +1,25 @@
 import asyncHandler from "express-async-handler";
 import User from "../Models/UserModel.js";
 import bcrypt from "bcryptjs";
+import { generateToken } from "../middlewares/Auth.js";
 //@desc register user
 //@route POST /api/users/
-//@access public 
+//@access public
 const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, password, image } = req.body;
   try {
     const userExists = await User.findOne({ email });
     //check user exists
-    if (userExists) { 
+    if (userExists) {
       res.status(400);
       throw new Error("User already exists");
     }
-   
 
-     //hash password
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
+    //hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-  
-      // create new user in DB 
+    // create new user in DB
     const user = await User.create({
       fullName,
       email,
@@ -37,13 +36,41 @@ const registerUser = asyncHandler(async (req, res) => {
         isAdmin: user.isAdmin,
         token: generateToken(user._id),
       });
-    }
-    else{
-       res.status(400);
-       throw new Error('Invalid user data'); 
+    } else {
+      res.status(400);
+      throw new Error("Invalid user data");
     }
   } catch (error) {
-    res.status(400).json({message: error.message});
+    res.status(400).json({ message: error.message });
   }
 });
-export { registerUser };
+
+//login user
+//route//post/users/login
+//access public
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    //find user in DB
+    const user = await User.findOne({ email });
+    //if user exist compare password with hashpassword and send user data and token to client
+    if (user && (await bcrypt.compare(password, user.password))) {
+      res.json({
+        _id: user._id,
+        fullName: user.fullName,
+        email: user.email,
+        image: user.image,
+        isAdmin: user.isAdmin,
+        token: generateToken(user._id),
+      });
+    }
+    //if user not found or password not match send error message
+    else {
+      res.status(401);
+      throw new Error("Invalid email or password");
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+export { registerUser,loginUser };
--- server/Routes/UserRouter.js
+++ server/Routes/UserRouter.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { registerUser } from '../Controllers/UserControllers.js';
+import { loginUser, registerUser } from '../Controllers/UserControllers.js';
 const router=express.Router();
 
 // **Public Routes
 router.post("/",registerUser);
+router.post("/login",loginUser);
 export default router;
